5ecc4fe88793f380c463f3bfc9ccfa292902595c
Marina-Chuhlib <marinachuhlib@gmail.com>
2023-02-23 18:30:01 +0300

    Update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 import { useSelector, useDispatch } from 'react-redux';
 
 
